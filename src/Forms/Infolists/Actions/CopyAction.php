47d8b0d2ad39b4ced707ce21a9dd06a9945ff992
m-kilic <kilic-muhammet@hotmail.com>
2024-05-17 04:07:13 +0300

    infolist copy

---
+++ src/Forms/Infolists/Actions/CopyAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Webbingbrasil\FilamentCopyActions\Forms\Infolists\Actions;
+
+use Webbingbrasil\FilamentCopyActions\Concerns\HasCopyable;
+use Filament\Actions\Action as BaseAction;
+
+class CopyAction extends BaseAction
+{
+    use HasCopyable;
+}
